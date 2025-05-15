10921006b16ff421db43fbb9189fb03b000b56fc
adf-evoketest <134678730+adf-evoketest@users.noreply.github.com>
2025-05-15 22:21:27 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project_name" {}
+variable "location" {}
+variable "environment" {}
+
+variable "enable_appservice" {
+  default = false
+}
+
+variable "enable_storage" {
+  default = false
+}
